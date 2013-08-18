2fa8824806b1134d19fa5ae61d0c8c9cba7ade47
Tagore Wang <wtao901231@gmail.com>
2013-08-18 13:38:53 +0800

    Initial Version
    Create the Android Project, and demonstrate the
    android.widget.SlidingDrawer uasge.

---
+++ src/me/wtao/widget/DemoActivity.java
@@ -0,0 +1,30 @@
+package me.wtao.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SlidingDrawer;
+
+public class DemoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.activity_demo);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.demo, menu);
+		return true;
+	}
+
+}
